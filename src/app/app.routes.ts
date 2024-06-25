67b108f1fa3967307527e6bc6dc68750ebc772e9
zanl0 <zanlo1781@gmal.com>
2024-06-24 21:53:31 -0500

    :sparkles:creacion de userslogin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"login",component:FormComponent}
+];
