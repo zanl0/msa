1c3f38d439aac97e94352c22c52d84a02aede0f4
miklegonza <gonzalez.michael1803@gmail.com>
2024-06-25 19:19:52 -0500

    :wrench: Fix distribución de carpetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './component/footer/footer.component';
-import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [RouterOutlet, FooterComponent, NavbarComponent],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
     title = 'msa';
