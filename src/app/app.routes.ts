ad663de891dfdff1639f84d8d219d00108dfff3f
miklegonza <gonzalez.michael1803@gmail.com>
2024-06-27 19:26:50 -0500

    :sparkles: Componentes admin/producto y dynamic/modal-content agregados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
+import { ProductosComponent } from './components/admin/productos/productos.component';
 
 export const routes: Routes = [
-    {path:"login",component:FormComponent}
+    { path: 'login', component: FormComponent },
+    { path: 'admin', component: ProductosComponent },
 ];
