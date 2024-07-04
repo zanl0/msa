36f8bce047b491b384cd9a1fdb5352f2539db4cd
sant20240 <sant1403@hotmail.com>
2024-07-03 20:45:21 -0500

    carrito santiago

---
+++ src/app/components/carrito/carrito.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-carrito',
+    standalone: true,
+    imports: [],
+    templateUrl: './carrito.component.html',
+    styleUrl: './carrito.component.scss'
+})
+export class CarritoComponent {
+
+}
+
+
