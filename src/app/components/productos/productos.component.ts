b72420904cc24f31fc36e88af387e86e8205202f
zanl0 <zanlo1781@gmal.com>
2024-07-03 21:59:54 -0500

    hrefs

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Product } from '../../models/productos';
 import { CarruselComponent } from '../carrusel/carrusel.component';
 import { ComponentesService } from '../../services/componentes/componentes.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-productos',
@@ -12,15 +13,16 @@ import { ComponentesService } from '../../services/componentes/componentes.servi
 })
 export class ProductosComponent {
 
-    productos: Product[] = [ ];
+    productos: Product[] = [];
 
-    constructor(private _api:ComponentesService) {}
-    ngOnInit(){
+    constructor(private _api: ComponentesService, protected router: Router) { }
+
+    ngOnInit() {
         this.getProductos();
     }
-    getProductos(){
-        this._api.getComponents().subscribe((data:any)=>{
-            this.productos=data;
+    getProductos() {
+        this._api.getComponents().subscribe((data: any) => {
+            this.productos = data;
         })
     }
 }
